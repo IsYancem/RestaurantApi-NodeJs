4ce23ec473d5784dd7e79086ed401b3d8691c8e2
Abraham Yance <122916029+IsYancem@users.noreply.github.com>
2025-05-26 16:56:48 -0500

    update restaurant api

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -51,4 +51,66 @@ const createRestuarantController = async (req, res) => {
   }
 };
 
-module.exports = { createRestuarantController };
+const getAllRestaurantsController = async (req, res) => {
+  try {
+    const restaurants = await restaurantModel.find({});
+    if (!restaurants) {
+      return res.status(404).send({
+        success: false,
+        message: "No restaurant Availible",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      totalCount: restaurants.length,
+      restaurants,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in GET ALL RESTAURANTS API",
+      error,
+    });
+  }
+};
+
+// GET RESTAURANT BY ID
+const getRestaurantyIdController = async (req, res) => {
+  try {
+    const restaurantId = req.params.id;
+    // find restaurant
+    const restaurant = await restaurantModel.findById(restaurantId);
+    if (!restaurant) {
+      return res.status(404).send({
+        success: false,
+        message: "No restaurant found",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      message: "Resturant found!!",
+      restaurant,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in GET Restaurant by Id",
+      error,
+    });
+  }
+};
+
+// DELETE RESTAURANT || DELETE
+const deleteUserController = async (req, res) => {
+
+}
+
+module.exports = {
+  createRestuarantController,
+  getAllRestaurantsController,
+  getRestaurantyIdController,
+  getRestaurantyIdController,
+  deleteUserController,
+};
--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const authMiddleware = require("../middleware/authMiddleware");
-const { createRestuarantController } = require("../controllers/restaurantController");
+const {
+  createRestuarantController,
+  getAllRestaurantsController,
+  getRestaurantyIdController,
+  deleteUserController,
+} = require("../controllers/restaurantController");
 
 const router = express.Router();
 
 // routes
 // CREATE RESTAURANT
-router.post("/create", authMiddleware, createRestuarantController)
+router.post("/create", authMiddleware, createRestuarantController);
+
+// GET ALL RESTAURANTS
+router.get("/getAll", authMiddleware, getAllRestaurantsController);
+
+// GET RESTAURANT BY id
+router.get("/get/:id", authMiddleware, getRestaurantyIdController);
+
+// DELETE RESTAURANT || DELETE
+router.delete("/delete/:id", authMiddleware, deleteUserController);
 
 module.exports = router;
